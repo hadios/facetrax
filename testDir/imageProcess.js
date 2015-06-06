b6883c68d975c5df73e2188e230faf68d74dba88
hadios <damienhadios@gmail.com>
2015-06-06 19:07:34 -0400

    Able to detect face from local files and crop multiple images from a single image.

--- testDir/imageProcess.js
+++ testDir/imageProcess.js
@@ -1,82 +1,107 @@
 var easyimg = require('easyimage');
 var request = require('request');
+var needle  = require('needle');
 
 var apiKeys = require('../api');
+
+var imageIndex  = 1;
+var imageLocals = ['babysanta1.jpg',
+                   'family.jpg'];
 
 // GLOBAL VARIABLES
 var CROPURL = "https://api.idolondemand.com/1/api/sync/detectfaces/v1";
 
-var cropImage = function (cropDetails, imageSource, imageDestination) {
+var cropImage = function (cropDetails, imageSource, imageDestination, cb) {
     easyimg.crop(
         {
-            src:'beach.jpg', dst:'beach-cropped.png',
-            cropwidth:128, cropheight:128,
-            gravity:'North',
-            x:30, y:50
+            src: imageSource, dst:imageDestination,
+            cropwidth:cropDetails.width, cropheight:cropDetails.height,
+            gravity:'NorthWest',
+            x:cropDetails.left, y:cropDetails.top
         }, function(err, stdout, stderr) {
-            if (err) throw err;
-            console.log('Cropped');
+            if (err || stdout || stderr) {
+                console.log("Crop error! Error: " + err);
+                return cb(err, null);
+            }
         }
     );
+
+    console.log('Cropped');
+    return cb(null);
 }
 
-
 var cropThisImage = function (source, cb) {
-    console.log("Cropping image!");
+    console.log("Preparing to crop image...");
 
+    // Read in the source image
     if (!source) {
         console.log("No image found!");
         return null;
     }
 
     var data = {
-        'form' : {
-            'apikey': apiKeys.HP_API_KEY,
-            'url': source,
-            'additional': true
-        }
+        'apikey': apiKeys.HP_API_KEY,
+        'file': {
+            'file': source,
+            'content_type': 'multipart/form-data'
+        },
+        'additional': true,
     };
 
-    request.post(
-        CROPURL,
-        data,
-        function (error, response, body) {
-            if (error) {
-                console.log("Error: " + error);
-                return;
-            }
+    needle.post(CROPURL,
+                data,
+                { multipart: true },
+                function (error, response, body) {
 
-            console.log(body);
-            //
-            // if (!error && response.statusCode == 200) {
-            //     console.log(body)
-            // }
-        }
+                if (error) {
+                    console.log("Error: " + error);
+                    return;
+                }
+
+                switch (response.statusCode) {
+                    case 200:
+                        console.log(body);
+
+                        var faces       = body.face;
+                        var numFaces    = faces.length;
+
+                        if (numFaces == 0) {
+                            console.log("C'mon put a face in there!");
+                            return false;
+                        } else {
+                            console.log("Extracting " + numFaces + "!");
+                        }
+
+                        // Extract all the faces from the photo
+                        for (var i = 0; i < faces.length; i++) {
+                            cropImage(faces[i], source, (i+1) + "_" + source, function(err){
+                                if (err) {
+                                    console.log("[ERROR]: Failed cropping...");
+                                    return false;
+                                }
+
+                                console.log("Image cropped!");
+                            });
+                        }
+                        break;
+
+                    default:
+                        console.log("Error in face detection. " + body);
+                        break;
+                }
+            }
     );
 
-    // Read in the source image
-
-    // Send to api for crop details
-
-
-
-    // Check if there is any faces
-        // Too many or no faces detected
-        // Return false;
-
-        // Prepare to crop image
-        // Crop image
-
-        // return cropped image link
-
-    return;
+    return false;
 }
 
-var source = 'https://www.idolondemand.com/sample-content/images/babysanta1.jpg';
-//source = './babysanta1.jpg';
+var source = imageLocals[imageIndex];
+
 cropThisImage(source, function(croppedImage) {
     if (!croppedImage) {
         console.log("Nothing to return");
         return;
     }
+
+    console.log("Succesfully crop image!");
 });
